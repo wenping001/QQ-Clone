a7a35fe3aee24d875c085947a67885188a83cf27
wping <wen240979@gmail.com>
2021-11-08 14:58:17 +0800

    Used shared preferences to implement remember password.

--- app/src/main/java/com/example/qq/LoginActivity.java
+++ app/src/main/java/com/example/qq/LoginActivity.java
@@ -1,27 +1,59 @@
 package com.example.qq;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 
 import androidx.appcompat.app.AlertDialog;
 
 public class LoginActivity extends BaseActivity{
-
+    private SharedPreferences pref;
+    private SharedPreferences.Editor editor;
     public Button login;
-    public EditText username;
-    public EditText password;
+    public EditText usernameEdit;
+    public EditText passwordEdit;
+    private CheckBox rememberPass;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        pref = PreferenceManager.getDefaultSharedPreferences(this);
+
         login = findViewById(R.id.login_btn);
-        username = findViewById(R.id.username);
-        password= findViewById(R.id.password);
+        usernameEdit = findViewById(R.id.username);
+        passwordEdit= findViewById(R.id.password);
+
+        rememberPass = findViewById(R.id.remember_password);
+        boolean isRemember = pref.getBoolean("remember_password",false);
+
+        if(isRemember){
+            String username = pref.getString("username","");
+            String password = pref.getString("password","");
+            usernameEdit.setText(username);
+            passwordEdit.setText(password);
+            rememberPass.setChecked(true);
+        }
+
         login.setOnClickListener(v->{
-            if(username.getText().toString().equals("admin") && password.getText().toString().equals("123456")){
+            String usernameStr = usernameEdit.getText().toString();
+            String passwordStr = passwordEdit.getText().toString();
+            if(usernameStr.equals("admin") && passwordStr.equals("123456")){
+                editor = pref.edit();
+                if(rememberPass.isChecked()){
+                    editor.putBoolean("remember_password",true);
+                    editor.putString("username",usernameStr);
+                    editor.putString("password",passwordStr);
+                }
+                else{
+                    editor.clear();
+                }
+                editor.apply();
+
                 Intent intent = new Intent(this,MainActivity.class);
                 startActivity(intent);
             }
